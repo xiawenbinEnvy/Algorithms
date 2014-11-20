b436e320d0b61d4e4c4883288b499b32b58434f0
xiawenbinEnvy <xiawenbin_xwb@163.com>
2014-11-20 17:56:33 +0800

    commit
    NFA中的“重复一次”和“重复n次”操作代码有问题，先删除

--- ConsoleApplication11/ConsoleApplication11/4字符串/子字符串操作/NFA.cs
+++ ConsoleApplication11/ConsoleApplication11/4字符串/子字符串操作/NFA.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using ConsoleApplication11._3图;
 
 namespace ConsoleApplication11
@@ -15,90 +14,6 @@ namespace ConsoleApplication11
         private DirectGraph G;//epsilon转换
         private int M;//状态数量,即正则表达式长度
 
-        private char[] Pre(char[] re)//处理‘+’至少重复一次
-        {
-            int M = re.Length;
-            Stack<int> stLeft = new Stack<int>();
-            for (int i = 0; i < M; i++)
-            {
-                if (re[i] == '(') stLeft.Push(i);
-                else if (i < M - 1 && re[i] == ')' && re[i + 1] != '+') stLeft.Pop();
-                else if (i < M - 1 && re[i] == ')' && re[i + 1] == '+')
-                {
-                    int left = stLeft.Pop();
-                    int right = i;
-
-                    int l = right - left + 1;
-                    char[] tmp = new char[M + l];
-                    for (int j = 0; j < M; j++)
-                    {
-                        tmp[j] = re[j];
-                    }
-                    for (int j = M - 1; j >= right; j--)
-                    {
-                        tmp[j + l] = tmp[j];
-                    }
-                    for (int j = 0; j < l; j++)
-                    {
-                        tmp[right + j + 1] = re[left + j];
-                    }
-                    tmp[right + l + 1] = '*';
-
-                    return Pre(tmp);
-                }
-            }
-            return re;
-        }
-
-        private char[] Pre2(char[] re, string reg)//处理‘{n}’指定重复次数
-        {
-            int M = re.Length;
-            Stack<int> stLeft = new Stack<int>();
-            for (int i = 0; i < M; i++)
-            {
-                if (re[i] == '(') stLeft.Push(i);
-                else if (i < M - 1 && re[i] == ')' && re[i + 1] != '{') stLeft.Pop();
-                else if (i < M - 1 && re[i] == ')' && re[i + 1] == '{')
-                {
-                    int j = -1;//}位置
-                    for (j = i + 1; j < M; j++)
-                    {
-                        if (re[j] == '}') break;
-                    }
-                    string sss = reg.Substring(i + 1, j - i);
-                    if (sss.Contains("-")) return re;
-                    int n = Convert.ToInt32(sss.Replace("{", "").Replace("}", "")) - 1;//重复次数
-
-                    int left = stLeft.Pop();
-                    int right = i;
-                    int l = right - left + 1;//要重复的部分的长度
-                    int ll = l * n;//总共要重复的长度
-
-                    int x = j - (i + 1) + 1;
-                    char[] tmp = new char[M + ll - x];
-
-                    for (int z = 0; z < M; z++)
-                    {
-                        tmp[z] = re[z];
-                    }
-                    for (int z = M - 1; z >= j + 1; z--)
-                    {
-                        tmp[z + ll - x] = re[z];
-                    }
-                    int postion = i + 1;
-                    for (int z = 1; z <= n; z++)
-                    {
-                        for (int zz = 0; zz < l; zz++)
-                        {
-                            tmp[postion] = re[left + zz];
-                            postion++;
-                        }
-                    }
-                    return Pre2(tmp, new String(tmp));
-                }
-            }
-            return re;
-        }
         /// <summary>
         /// 根据给定的正则表达式构造NFA
         /// </summary>
@@ -106,8 +21,6 @@ namespace ConsoleApplication11
         {
             Stack<int> ops = new Stack<int>();//用来保存左括号、或符号
             re = regexp.ToCharArray();
-            re = Pre(re);
-            re = Pre2(re, regexp);
             M = re.Length;
             G = new DirectGraph(M + 1);//有向图，保存每个点出发可以到达的点
 
